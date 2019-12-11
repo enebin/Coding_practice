7eb24091e6ba33cf6e97ca53ca145028f3ed2bfc
enebin <46271447+enebin@users.noreply.github.com>
2019-12-11 10:43:36 +0900

    mod

--- GradingStudent.py
+++ GradingStudent.py
@@ -2,7 +2,9 @@ from math import floor
 
 def gradingStudents(grades):
     for i in range(len(grades)):
-        if grades[i] % 5 >= 3:
+        if grades[i] < 38:
+            pass
+        elif grades[i] % 5 >= 3:
             grades[i] -= grades[i] % 5
             grades[i] += 5
         else :
