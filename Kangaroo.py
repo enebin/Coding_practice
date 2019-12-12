c59a8d950d488453e0c62a6011bf9edb1612cce2
enebin <46271447+enebin@users.noreply.github.com>
2019-12-12 16:22:51 +0900

    Update Kangaroo.py

--- Kangaroo.py
+++ Kangaroo.py
@@ -1,3 +1,4 @@
+'''
 def kangaroo(x1, v1, x2, v2):
     if x1 > x2:
         x = x2
@@ -6,7 +7,6 @@ def kangaroo(x1, v1, x2, v2):
         v = v2
         v2 = v1
         v1 = v
-
         print(x1, v1, x2, v2)
 
     if v2 > v1: 
@@ -15,14 +15,28 @@ def kangaroo(x1, v1, x2, v2):
         n = 1
         first = x1 + v1*n
         second = x2 + v2*n
-        while first != second or first < second :
+        while first != second and first < second :
             n += 1
             first = x1 + v1*n
             second = x2 + v2*n
+            print(first, second)
 
         if first > second: 
             return "NO"
         elif first == second:
             return "YES"
-       
-print(kangaroo(10,9,9,1))
+'''
+
+def kangaroo(x1, v1, x2, v2):
+    if x2 > x1 and v2 > v1:
+        return "NO"
+    elif x1 > x2 and v1 > v2:
+        return "NO"
+    elif abs(v1 - v2):
+        return "NO"
+    elif not abs(x1 - x2) % abs(v1 - v2):
+        return "YES"
+    else:
+        return "NO"
+
+print(kangaroo(2,3,3,2))
