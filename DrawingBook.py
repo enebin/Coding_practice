c3f3f64de1af10b7d17e267444113ebed40d2f20
enebin <46271447+enebin@users.noreply.github.com>
2019-12-15 17:36:14 +0900

    m

---
+++ DrawingBook.py
@@ -0,0 +1,2 @@
+def pageCount(n, p):
+    
--- SockMerchant.py
+++ SockMerchant.py
@@ -1,4 +1,4 @@
 def sockMerchant(n, ar):
-    return sum(ar.count(i) % 2 == 0 for i in arr)
+    return sum([ar.count(i) // 2 for i in set(ar)])
 
 print(sockMerchant(9, [10, 20, 20, 10, 10, 30, 50, 10, 20]))
