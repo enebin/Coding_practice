04b44e7188b70f65502f2c6022f2ee09d713e253
enebin <46271447+enebin@users.noreply.github.com>
2019-12-21 20:28:33 +0900

    Update ClimbingLeaderboard.py

--- ClimbingLeaderboard.py
+++ ClimbingLeaderboard.py
@@ -1,5 +1,15 @@
-def climbingLeaderboard(scores, alice):
+def climbingLeaderboard_slow(scores, alice):
     count = []
     for i in alice:
         count.append(1 + len(set(scores)) - sum(i >= j for j in set(scores)))
     return count
+
+def climbingLeaderboard(scores, alice):
+    length = len(scores)
+    f = lambda x : x // 2
+    for i in alice:
+        if scores[f(length)] > i:
+            length = f(length)
+
+        else:
+
