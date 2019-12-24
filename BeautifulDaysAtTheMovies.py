1518fb3affd221bef065d8cd3c0713e310f3ee80
enebin <46271447+enebin@users.noreply.github.com>
2019-12-24 13:48:11 +0900

    Update BeautifulDaysAtTheMovies.py

--- BeautifulDaysAtTheMovies.py
+++ BeautifulDaysAtTheMovies.py
@@ -1,2 +1,5 @@
 def beautifulDays(i, j, k):
-    return sum(l -int(list(l).reverse))  % k == 0 for l in range(i, j))
+    return sum(abs(l - int(''.join(list(reversed(list(str(l))))))) % k == 0 for l in range(i, j+1))
+
+
+print(beautifulDays(20, 23, 6))
