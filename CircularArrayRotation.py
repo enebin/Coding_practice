81d6d40df1a9773be5bacb8ebd3d52378c9e05f0
enebin <46271447+enebin@users.noreply.github.com>
2019-12-25 20:22:03 +0900

    Update CircularArrayRotation.py

--- CircularArrayRotation.py
+++ CircularArrayRotation.py
@@ -1,2 +1,7 @@
 def circularArrayRotation(a, k, queries):
-    
+    res = []
+    for i in range(len(a)):
+        res[i] = a[i - (k % len(a))]
+    return res    
+
+print(circularArrayRotation([1,2,3],2,[0,1,2]))
