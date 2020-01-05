da843194b41d8bc590ae38411836e4484b9d8e8c
enebin <46271447+enebin@users.noreply.github.com>
2020-01-05 20:14:35 +0900

    s

---
+++ C/Baek/1011.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int iter;
+    scanf("%d", &iter);
+
+    while(iter--){
+        int x, y;
+        int result = 0;
+
+        scanf("%d %d", &x, &y);
+
+        if (y-x == 1){
+            printf("%d", 1);
+        }
+        else{
+            int plus = 2;
+            int j = 0;
+
+            while (++j){
+                result += plus;
+                if (j % 2 == 0)
+                    plus++;
+                if (y-x > result){
+                    printf("%d", j);
+                    break;
+                }
+            }
+        }
+    }
+    return 0;
+}
+
