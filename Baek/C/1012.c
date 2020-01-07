9effa4e7bb23df9b952a6f4f1406b8d42d57afd3
enebin <46271447+enebin@users.noreply.github.com>
2020-01-07 13:51:56 +0900

    Update 1012.c

--- Baek/C/1012.c
+++ Baek/C/1012.c
@@ -1,13 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct lettuce{
+    int 
+
+}
+
 int main()
 {
     int iter;
     scanf("%d", &iter);
 
     while(iter--){
-        
+        int n, m, k;
+        scanf("%d %d %d", &n, &m, &k);
+
+        int x, y;
+        for (int i=0; i<k; i++){
+            scanf("%d %d", &x, &y);
+            loc[x][y] = 1;
+        }
+
     }
 
     return 0;
