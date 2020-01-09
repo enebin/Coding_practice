ad0d3e9e2c70e8d5063e0c99fd7969d1f8f08dda
enebin <46271447+enebin@users.noreply.github.com>
2020-01-09 18:33:52 +0900

    f

--- Baek/C/9465.c
+++ Baek/C/9465.c
@@ -1,35 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int up_score[100000][2];
+int up_score[100001][2];
 
 int main()
 {
     int iter;
     scanf("%d", &iter);
 
-    while(iter--){
-        int n;
-        scanf("%d", &n);
+    while(iter--) {
+        if (iter % 2){
+            
 
-        for (int i=0; i<n; i++){
-            scanf("%d", &up_score[i]);
         }
-        for (int i=0; i<n; i++){
-            scanf("%d", &dn_score[i]);
+        else {
+            
         }
-
-        int sum1 = 0;
-        int sum2 = 0;
-
-        for (int i=0; i<=n/2; i++){
-            sum1 += up_score[i*2] + dn_score[i*2 + 1];
-            sum2 += up_score[i*2 + 1] + dn_score[i*2]; 
-        }
-
-        printf("%d\n", sum1 > sum2 ? sum1 : sum2);
-         
+        
     }
+     
 
     return 0;
 }
