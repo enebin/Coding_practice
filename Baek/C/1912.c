ed605eb22fcec36dcf7c2401021c2ad1f369304f
enebin <46271447+enebin@users.noreply.github.com>
2020-01-12 09:42:51 +0900

    m

--- Baek/C/1912.c
+++ Baek/C/1912.c
@@ -3,8 +3,6 @@
 
 int x[1000001] = {0};
 int res[1000001] = {0}; 
-int temp;
-int max = 0;
 
 int comp (int x, int y){
     return x > y ? x : y ;
@@ -15,26 +13,19 @@ int main()
     int iter;
     scanf("%d", &iter);
 
-    int j = 1;
     for(int i=1; i<=iter; i++){
-
-        scanf("%d", &temp);
-        
-        if (temp >= 0){
-            x[j] += temp;
-        }
-        else{
-            x[++j] = temp; 
-            j++;
-        }
+        scanf("%d", &x[i]);
     }
 
-    if (x[1] < 0)
-        x[1] = 0;
+    res[1] = x[1];
+    int max = res[1];
 
-    for(int i=2; i<=j; i++){
-        if (max < comp(res[i] - ))
+    for (int i=2; i<=iter; i++){
+        res[i] = comp(res[i-1] + x[i], x[i]);
+        max = comp(max, res[i]);
     }
+
+    printf("%d\n", max);
 
     return 0;
 }
