30e932153c57f6c746c9a1ff75bef60d2e0dcaa8
enebin <46271447+enebin@users.noreply.github.com>
2020-01-14 16:34:15 +0900

    M

--- Baek/C/10799.c
+++ Baek/C/10799.c
@@ -1,16 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+char n[1000001];
+int stack = 0;
+int count = 0;
 
 int main()
 {
-    int iter;
-    scanf("%d", &iter);
+    scanf("%s", n);
+    int len = strlen(n);
 
-    for(int i=1; i<=iter; i++){
-        scanf("%d", &iter);
+    for (int i=0; i<len; i++){
+        if (n[i] == '(')
+            stack++;
+        else {
+            if (n[i-1] == '('){
+                count += --stack;
+            }
+            else{
+                stack--;
+                count++;
+            }
+            
+        }
     }
 
-    printf("%d", &iter);
-
+    printf("%d\n", count);
     return 0;
 }
