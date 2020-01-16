3a9f7d43dd38ebef8704ddc0a123d9c89fbf65a8
enebin <46271447+enebin@users.noreply.github.com>
2020-01-16 15:28:46 +0900

    m

--- Baek/C/11656.c
+++ Baek/C/11656.c
@@ -1,22 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
+char res[1000][1000];
+
 int comp(const void* a, const void* b){
-    char* n1
+    char* c1 = (char*) a;
+    char* c2 = (char*) b;
+
+    int diff = strcmp(c1, c2);
+    
+    if (diff)
+        return diff;
 }
 
 int main()
 {
     char str[1000];
-    char lux[1000][2];
     gets(str);
 
     int len = strlen(str);
-    for(int i=0; i<len; i++){
-        for (int j=i; j<len; j++){
-            lux[j][i] = str[j];
+
+    for (int i=0; i<len; i++){
+        for (int j=i; j<=len; j++){
+            res[i][j] = str[j];
         }
     }
+
+    for (int i=0; i<len; i++)
+        printf("%s\n", res[i]);
+
+    qsort(res, len, 1000*sizeof(char), comp);
+
+    for (int i=0; i<len; i++)
+        printf("%s", res[i]);
 
     return 0;
 }
