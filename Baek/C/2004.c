4b6d88a91c4323ab291fad4404c210a8a21ec708
enebin <46271447+enebin@users.noreply.github.com>
2020-01-20 17:11:43 +0900

    Update 2004.c

--- Baek/C/2004.c
+++ Baek/C/2004.c
@@ -1,23 +1,45 @@
 #include <stdio.h>
-
-int main()
-{
-    int iter;
-    scanf("%d", &iter);
-    
-    int res = 1;
+#define min(a,b) (a<b?a:b)
+ 
+long long int i;
+int n, r;
+int five = 0, two = 0;
+ 
+int fiveCount(int num) {
     int count = 0;
-    for (int i=1; i<=iter; i++){
-        res *= i;
-        while (res % 10 == 0){
-            count++;
-            res /= 10;
-        }
-        res %= 1000;
-    }
-
-
-    printf("%d\n", count);
-
+    
+    for (i = 5; num / i >= 1; i *= 5)
+        count += num / i;
+    
+    return count;
+}
+ 
+int twoCount(int num) {
+    int count = 0;
+    
+    for (i = 2; num / i >= 1; i *= 2)
+        count += num / i;
+    
+    return count;
+}
+ 
+int main() {
+    
+    scanf("%d %d", &n, &r);
+ 
+ 
+    five += fiveCount(n);
+    if (r != 0)five -= fiveCount(r);
+    if (n != r)five -= fiveCount(n - r);
+ 
+    two += twoCount(n);
+    if (r != 0)two -= twoCount(r);
+    if (n != r)two -= twoCount(n - r);
+ 
+    printf("%d\n", min(two, five));
+ 
     return 0;
 }
+             
+
+
