a1eebd8f7f07f701485d071d252a76672512cc74
enebin <46271447+enebin@users.noreply.github.com>
2020-01-24 09:22:38 +0900

    Update 1654.c

--- Baek/C/1654.c
+++ Baek/C/1654.c
@@ -3,18 +3,37 @@
 
 typedef long long int lld;
 
-lld lans[10000];
-
-int max_len = LONG_MAX;
+lld lans[10001];
 
 int main()
 {
     int k, n;
     scanf("%d %d", &k, &n);
 
+    int max = 0;
     for(int i=1; i<=k; i++){
-        scanf("%d", &lans[i]);
+        scanf("%lld", &lans[i]);
+        max = max < lans[i] ? lans[i] : max;
     }
+
+    lld high = max;
+    lld low = 1;
+    lld mid = 1;
+
+    while (high >= low){
+        mid = (high + low) / 2;
+
+        int res = 0;
+        for (int i=1; i<=k; i++)
+            res += lans[i] / mid;
+        
+        if (res >= n)
+            low = mid + 1;
+        else 
+            high = mid - 1;
+    }
+
+    printf("%d\n", low-1);
 
     return 0;
 }
