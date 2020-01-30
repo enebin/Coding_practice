22d3d6692ad56e3f40f6b8ebd466a9de1e65c2f9
enebin <46271447+enebin@users.noreply.github.com>
2020-01-30 16:01:27 +0900

    Update 1107.cpp

--- Baek/C/1107.cpp
+++ Baek/C/1107.cpp
@@ -1,13 +1,52 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
-int cur = 100;
+bool broken[10];
+
+int check(int n){
+    if (n == 0){
+        if (broken[0])
+            return 0;
+        else 
+            return 1;
+    }
+
+    int len = 0;
+    while (n > 0){
+        if (broken[n % 10]) 
+            return 0;
+        n /= 10;
+        len++;
+    }
+    return len;
+}
 
 int main(){
-    int iter;
-    cin >> iter;
+    int n, m;
+    cin >> n >> m;
 
-    
+    for (int i=0; i<m; i++){
+        int j;
+        cin >> j;
+        broken[j] = true;
+    }
 
+    int result = abs(n - 100);
+    for (int i=0; i<=1000000; i++){
+        int c = i;
+        int len = check(c);
+
+        if (len > 0){
+            int press = abs(c - n);
+            if (result > press + len){
+                result = press + len;
+            }
+        }
+    }
+
+    cout << result << endl;
+    return 0;
 }
+
