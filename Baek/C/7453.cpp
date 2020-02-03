698e4fe8c245c68f8c37ee0f769a925ff30d9f9b
enebin <46271447+enebin@users.noreply.github.com>
2020-02-03 16:20:44 +0900

    Update 7453.cpp

---
+++ Baek/C/7453.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int mat[4001][4001];
+
+
+int main(){
+    int iter;
+    cin >> iter;
+
+    for (int i=0; i<iter; i++){
+        for (int j=0; j<iter; j++){
+            cin >> mat[i][j];
+        }
+    }
+
+    
+
+}
