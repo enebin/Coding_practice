779be7d498328c540c6c8ea1d5d608827dc7f6fd
enebin <46271447+enebin@users.noreply.github.com>
2020-02-04 11:43:14 +0900

    Update 7453.cpp

--- Baek/C/7453.cpp
+++ Baek/C/7453.cpp
@@ -3,20 +3,46 @@
 #include <vector>
 
 using namespace std;
+typedef long long int lld; 
 
-int mat[4001][4001];
+int A[4001], B[4001], C[4001], D[4001];
+vector<int> AB, CD;
 
+vector<int>::iterator itr;
 
 int main(){
     int iter;
     cin >> iter;
 
     for (int i=0; i<iter; i++){
+        cin >> A[i] >> B[i] >> C[i] >> D[i];
+    }
+
+    for (int i=0; i<iter; i++){
         for (int j=0; j<iter; j++){
-            cin >> mat[i][j];
+            AB.push_back(A[i] + B[j]);
+            CD.push_back(C[i] + D[j]);
         }
     }
 
-    
+    sort(AB.begin(), AB.end());
+    sort(CD.begin(), CD.end());
+
+    lld result = 0;
+    for (int i=0; i<AB.size(); i++){
+        int low = lower_bound(CD.begin(), CD.end(), AB[i]*(-1)) - CD.begin();
+        int high = upper_bound(CD.begin(), CD.end(), AB[i]*(-1)) - CD.begin();
+        result += high - low;
+    }
+
+    cout << result << endl;
+    /*for (itr=AB.begin(); itr!=AB.end(); ++itr){
+        cout << *itr << " ";
+    }
+
+    cout << endl; 
+    for (itr=CD.begin(); itr!=CD.end(); ++itr){
+        cout << *itr << " ";
+    }*/
 
 }
