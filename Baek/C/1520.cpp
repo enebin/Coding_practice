23a73536cfb70580468f8c57a9aaf8d40269b8e7
enebin <46271447+enebin@users.noreply.github.com>
2020-02-24 21:49:41 +0900

    m

--- Baek/C/1520.cpp
+++ Baek/C/1520.cpp
@@ -1,42 +1,47 @@
-#include <iostream>
-#include <algorithm>
-#include <vector>
-
-using namespace std;
-
-int arr[501][501];
-int visited[501][501] = {0, };
-int n, m;
-
-void dfs(int x, int y){
-    int count = 1;
-    visited[x][y] = count;
-    int targ = arr[x][y];
-
-    if (arr[x+1][y] < targ && !visited[x+1][y])
-        dfs(x+1, y);
-    if (arr[x][y+1] < targ && !visited[x][y+1])
-        dfs(x, y+1);
-    if (arr[x-1][y] < targ && !visited[x-1][y])
-        dfs(x-1, y);
-    if (arr[x][y-1] < targ && !visited[x][y-1])
-        dfs(x, y-1);
-    
-    if (x == n && y)
-
-
+#include <stdio.h>
+ 
+int map[510][510] = {};
+int visit[510][510] = {};
+int M, N;
+ 
+struct saveMap{
+    int x;
+    int y;
+};
+ 
+saveMap make_saveMap(int x, int y){
+    saveMap temp;
+    temp.x = x; temp.y = y;
+    return temp;
 }
-
-int main(){
-    cin >> n >> m;
-
-    for (int i=1; i<=n; i++){
-        for (int j=1; j<=m; j++){
-            scanf("%d", &arr[i][j]);
+ 
+int serch(int x, int y){
+ 
+    int vect_x[5] = { 0, 1, 0, -1, 0 };
+    int vect_y[5] = { 0, 0, 1, 0, -1 };
+ 
+    if (x == N && y == M)
+        return 1;
+    if (visit[y][x]) return visit[y][x];
+    for (int i = 1; i <= 4; i++){
+        int nextX = x + vect_x[i];
+        int nextY = y + vect_y[i];
+        if (map[nextY][nextX] < map[y][x] && nextX>0 && nextX <= N && nextY>0 && nextY <= M){
+ 
+            visit[y][x] += serch(nextX,nextY);
+ 
         }
     }
-
-    for (int i=1; i<=n; i++){
-
-    }
+ 
+    return visit[y][x];
+}
+int main(void){ 
+    scanf("%d %d", &M, &N);
+ 
+    for (int i = 1; i <= M; i++)
+        for (int j = 1; j <= N; j++)
+            scanf("%d", &map[i][j]); 
+    
+    printf("%d\n", serch(1,1));
+    return 0;
 }
