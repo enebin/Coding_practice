086460bd19d0071223561fcc5f8c7792f8053948
enebin <46271447+enebin@users.noreply.github.com>
2020-02-28 16:31:13 +0900

    m

--- Baek/C/2618.cpp
+++ Baek/C/2618.cpp
@@ -3,8 +3,8 @@
 #include <algorithm>
 
 using namespace std;
-vector<int> result;
-pair<int, int> p1, p2;
+vector<int> car1, car2;
+vector<pair<int, int>> p;
 
 int dp[1001][1001];
 
@@ -15,40 +15,16 @@ int calc(int x, int y, const int x_, const int y_){
 int main(){
     int n;
     scanf("%d", &n);
-    
-    p1.first = 1, p1.second = 1;
-    p2.first = n, p2.second = n;
 
     int m;
     scanf("%d", &m);
 
-    int r = 0;
     while(m--){
         int x, y;
         scanf("%d %d", &x, &y);
-
-        int px1, px2, py1, py2, r1, r2;
-
-        px1 = p1.first, py1 = p1.second;
-        px2 = p2.first, py2 = p2.second;
-
-        r1 = calc(px1, py1, x, y), r2 = calc(px2, py2, x, y);
-
-        if (r1 < r2){
-            result.push_back(1);
-            p1 = make_pair(x, y);
-            r += r1;
-        }
-        else{
-            result.push_back(2);
-            p2 = make_pair(x, y);
-            r += r2;
-        }
+        p.push_back(make_pair(x, y));
     }
+    
+    
 
-    printf("%d\n", r);
-
-    for (auto iter = result.begin(); iter != result.end(); iter++){
-        printf("%d\n", *iter);
-    }
 }
