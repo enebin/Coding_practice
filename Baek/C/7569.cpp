6bb4bc2e5a5194f777611b76f19bfb99e887116c
enebin <46271447+enebin@users.noreply.github.com>
2020-03-02 16:21:47 +0900

    Update 7569.cpp

--- Baek/C/7569.cpp
+++ Baek/C/7569.cpp
@@ -4,6 +4,7 @@
 
 using namespace std;
 
+int n, m, s;
 int tom[101][101][101];
 int visit[101][101][101];
 int dx[6] = {0, 0, 0, 0, 1, -1};
@@ -13,13 +14,41 @@ int dz[6] = {1, -1, 0, 0, 0, 0};
 queue<pair<pair<int, int>, int>> q;
 
 int bfs(){
+    int day = 0;
 
+    while (!q.empty()){
+        int cur_size = q.size();
+
+        while(cur_size--){
+        
+        int x = q.front().first.first;
+        int y = q.front().first.second;
+        int z = q.front().second;
+
+        for (int i=0; i<6; i++){
+            int x_ = x + dx[i];
+            int y_ = y + dy[i];
+            int z_ = z + dz[i];
+
+            if (0<= x_ && x_ < n && 0 <= y_ && y < m && 0 <= z && z < s){
+                if (tom[x_][y_][z_] == 0){
+                    tom[x_][y_][z_] = 1;
+                    q.push(make_pair(make_pair(x_, y_), z_));
+                }
+            }
+            q.pop();
+        }
+        }
+
+
+        
+        
+    }
 
 
 }
 
 int main(){
-    int n, m, s;
     cin >> n >> m >> s;
 
     for (int k=0; k<s; k++){
