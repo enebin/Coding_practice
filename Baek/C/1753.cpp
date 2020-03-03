08480146a241d74e58b1c96e0effdc579cb04c30
enebin <46271447+enebin@users.noreply.github.com>
2020-03-03 14:37:35 +0900

    Create 1753.cpp

---
+++ Baek/C/1753.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+int n, m;
+int INF = INT32_MAX;
+
+vector<pair<int, int>> map;
+
+int main(){
+    cin >> n >> m;
+
+    int start;
+    cin >> start;
+
+    for (int i=0; i<m; i++){
+
+    }
+    
+
+}
